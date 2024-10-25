3a0eb30c87731f684a26bd1cd30ab295b0dbee67
GrigoryanDav <grigoryand219@gmail.com>
2024-10-25 19:39:12 +0400

    Set up routing with DayForecast and HourlyForecast components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
+import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route } from "react-router-dom";
+import DayForecast from "./components/DayForecast";
+import HourlyForecast from "./components/HourlyForecast";
+import './styles/global.css'
+
+
 const App = () => {
   return (
     <div>
-    
+      <RouterProvider
+        router={
+          createBrowserRouter(
+            createRoutesFromElements(
+              <>
+                <Route path="/" element={<DayForecast />} />
+                <Route path="/:day" element={<HourlyForecast />} />
+              </>
+            )
+          )
+        }
+      />
     </div>
   );
 }
