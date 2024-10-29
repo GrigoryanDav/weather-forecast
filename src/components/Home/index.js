124ce5cda4534ceec16cfc34cc6d8698ec38d126
GrigoryanDav <grigoryand219@gmail.com>
2024-10-29 21:29:50 +0400

    Implement city checking functionality and add handleKeyDown function for button in Home

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,5 +1,6 @@
 import { useParams, useNavigate } from "react-router-dom"
 import { useState, useEffect } from "react"
+import { BASE_URL, API_KEY } from "../../utils/constants"
 import './index.css'
 
 
@@ -17,19 +18,39 @@ const Home = () => {
         setInputValue(e.target.value.trim())
     }
 
-    const handleSearch = () => {
-        navigate(`/${inputValue}`)
+    const checkCityExists = async () => {
+        try {
+            const response = await fetch(
+                `${BASE_URL}/weather?q=${inputValue}&appid=${API_KEY}`
+            )
+            if(response.ok) {
+                navigate(`/${inputValue}`)
+            } else {
+                alert("City not found. Please enter a valid city name.")
+            }
+        } catch (error) {
+            console.error("Error checking city:", error)
+            alert(`"An error occurred. Please try again later."`)
+        }
+    }
+
+    const handleKeyDown = (e) => {
+        if (e.key === 'Enter') {
+            checkCityExists()
+        }
     }
 
     return (
         <div className="home_container">
             <h2>Write the name of the city and find out the weather there for the next 5 days</h2>
-            <input 
-            type="text"
-            value={inputValue}
-            onChange={handleInputChange}
+            <input
+                type="text"
+                value={inputValue}
+                onKeyDown={handleKeyDown}
+                onChange={handleInputChange}
+                placeholder="Enter city name"
             />
-            <button onClick={handleSearch}>
+            <button onClick={checkCityExists}>
                 Search
             </button>
         </div>
